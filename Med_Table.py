b97af740da12426bb720a2615589a71cdf559489
Rahul Dewangan <96685874+rahul2001dewangan@users.noreply.github.com>
2022-10-27 16:16:19 +0530

    Update Med_Table.py

--- Med_Table.py
+++ Med_Table.py
@@ -5,4 +5,4 @@ conn = sqlite3.connect('Med_Table.db')
 db = conn.cursor()
 
 db.execute('''Create table Compositions (ID TEXT, Composition_name TEXT, Generic_comp_eq TEXT, PRIMARY KEY (ID));''')
-db.execute('''Create table Medicines(med_id TEXT, Medicine_name TEXT, [Manufacturer Comp_ID] TEXT, FOREIGN KEY ([Manufacturer Comp_ID]) REFERENCES Compositions(ID));''')
+db.execute('''Create table Medicines(med_id TEXT, Medicine_name TEXT, Manufacturer TEXT, Comp_ID TEXT, FOREIGN KEY (Comp_ID) REFERENCES Compositions(ID));''')
